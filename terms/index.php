6b6b90279d02c15da3319ababf07e471a6a0d21a
blha303 <stevensmith.ome@gmail.com>
2014-03-29 04:17:46 +0800

    I hate semicolons.

--- terms/index.php
+++ terms/index.php
@@ -2,5 +2,5 @@
 $title = "Terms";
 $content = "<div class=\"container\"><pre>";
 $content .= file_get_contents("../TERMS.txt");
-$content .= "</pre></div>"
+$content .= "</pre></div>";
 include("/home/puzl/website/template.php");
