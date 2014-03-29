7cbd5aa1ed8840bb5c15ae701d5c2d64174203ce
blha303 <stevensmith.ome@gmail.com>
2014-03-30 06:43:44 +0800

    Added twitter links

--- about/index.php
+++ about/index.php
@@ -10,33 +10,33 @@ $content = <<<EOF
       </ul>
       <div id="jobList" class="tab-content">
         <div class="tab-pane fade active in" id="FrontOffice">
-          <p><b>Trent Hensler</b> <a href="https://twitter.com/drtshock"><i class="fa fa-twitter"></i></a>
+          <p><b>Trent Hensler</b> <a href="https://twitter.com/drtshock"><i class="fa fa-twitter"></i>drtshock</a>
           <br /><i>President</i>
           <br />Trent owns and founded Puzl Inc. He develops for Puzls main account, Minetime LLC.</p>
           <br />
 
-          <p><b>Brandon Hensler</b>
+          <p><b>Brandon Hensler</b> <a href="https://twitter.com/brandonhensler"><i class="fa fa-twitter"></i>brandonhensler</a>
           <br /><i>Vice President - Operations</i>
           <br />Brandon assists in the day to day activity of the company.</p>
 
         </div>
         <div class="tab-pane fade" id="Developers">
-          <p><b>Steven Smith</b>
+          <p><b>Steven Smith</b> <a href="https://twitter.com/blha303"><i class="fa fa-twitter"></i>blha303</a>
           <br /><i>Systems Administrator</i>
           <br />Steven works on any backend server work that Puzl needs. He also assisted in setting up the website.</p
           <br />
 
-          <p><b>eueln</b>
+          <p><b>eueln</b> <a href="https://twitter.com/eueln"><i class="fa fa-twitter"></i></a>
           <br /><i>Java Developer</i>
           <br />Eueln writes Bukkit Plugins.</p>
           <br />
 
-          <p><b>DSH105</b>
+          <p><b>DSH105</b> <a href="https://twitter.com/_DSH105_"><i class="fa fa-twitter"></i></a>
           <br /><i>Java Developer</i>
           <br />DSH105 writes Bukkit Plugins.</p>
           <br />
 
-          <p><b>Chase</b>
+          <p><b>Chase</b> <a href="https://twitter.com/chaseoes"><i class="fa fa-twitter"></i>chaseoes</a>
           <br /><i>Java Developer</i>
           <br />Chase writes Bukkit Plugins.</p>
           <br />
