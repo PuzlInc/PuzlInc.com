de03b76a928d4db324a33c782d6d77a9fe2417e5
Justin <justin@battleplugins.com>
2014-07-18 22:14:22 -0400

    Push all changes

--- website/app/config/staff.php
+++ website/app/config/staff.php
@@ -9,23 +9,59 @@ return array(
 				'role' => "President",
 				'twitter' => 'drtshock',
 				'avatar' => '/assets/img/staff/1.png'
-			)
+			),
+			array(
+				'name' => 'Adan Fendley',
+				'role' => "Project Manager",
+				'twitter' => '#',
+				'avatar' => '/assets/img/staff/2.png'
+			),
+			array(
+				'name' => 'Stephanie Pyper',
+				'role' => "Operations Manager",
+				'twitter' => '#',
+				'avatar' => '/assets/img/staff/2.png'
+			),
 		)
 	),
 	array(
 		'name' => 'Developers',
 		'members' => array(
 			array(
-				'name' => 'Steven Smith',
-				'role' => 'Systems Administrator',
-				'twitter' => 'blha303',
-				'avatar' => '/assets/img/staff/3.png'
-			),
-			array(
 				'name' => 'Travis Ralston',
 				'role' => 'Java Developer',
 				'twitter' => 'turt2live',
 				'avatar' => '/assets/img/staff/5.png'
+			),
+			array(
+				'name' => 'Nate Mortensen',
+				'role' => 'Java Developer',
+				'twitter' => '#',
+				'avatar' => '/assets/img/staff/3.png'
+			),
+			array(
+				'name' => 'Conner James',
+				'role' => 'Java Developer',
+				'twitter' => '#',
+				'avatar' => '/assets/img/staff/3.png'
+			),
+			array(
+				'name' => 'Catalin Ionescu',
+				'role' => 'Java Developer',
+				'twitter' => '#',
+				'avatar' => '/assets/img/staff/3.png'
+			),
+			array(
+				'name' => 'Jonas Seibert',
+				'role' => 'Java Developer',
+				'twitter' => '#',
+				'avatar' => '/assets/img/staff/3.png'
+			),
+			array(
+				'name' => 'Steven Smith',
+				'role' => 'Python Developer',
+				'twitter' => 'blha303',
+				'avatar' => '/assets/img/staff/3.png'
 			),
 			array(
 				'name' => 'Justin Rosenthal',
